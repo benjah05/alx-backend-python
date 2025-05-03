df3fb9b6825d2251fbcde0a28ca8468d99bc5478
benjah05 <senzelebenjamine@gmail.com>
2025-05-03 10:18:21 +0000

    execution time

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -15,5 +15,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     delays = []
     tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    return sorted(delays)
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+let's measure the runtime
+"""
+
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    measure_time: measure the total executime time of wait_n
+    """
+    start_time = time.time()
+    run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = start_time - end_time
+    return total_time / n
