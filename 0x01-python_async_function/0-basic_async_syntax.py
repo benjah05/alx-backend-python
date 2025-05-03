504a160c12f5ea5eeaf29e0d74537e1ef21a689c
benjah05 <senzelebenjamine@gmail.com>
2025-05-03 08:33:49 +0000

    the basics of async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,7 +10,7 @@ from typing import Generator
 
 async def wait_random(max_delay: int = 10) -> Generator[None, None, float]:
     """
-    main: create asynchronous coroutine
+    wait_random: create asynchronous coroutine
     """
     delay_time = random.uniform(0, max_delay)
     await asyncio.sleep(delay_time)
